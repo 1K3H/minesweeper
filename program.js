0610856824e69f5f08df7ef21dd1aee89487ca07
1K3H <1K3H@users.noreply.github.com>
2020-11-21 06:57:47 +0900

    입력값을 범위로 하는 중복없는 난수 배열 생성

--- program.js
+++ program.js
@@ -4,10 +4,13 @@ startBtn.addEventListener("click", setGame);
 function setGame() {
     const row = parseInt(document.getElementById("row").value);
     const col = parseInt(document.getElementById("col").value);
-    
+    const mineNum = parseInt(document.getElementById("mineNum").value);
+
     makeBoard(row, col);
-    
-    function makeBoard(rowNum, colNum) {
+    setMineNumArr(mineNum, row * col);
+}
+
+function makeBoard(rowNum, colNum) {
     let tableEle = '<table>';
 
     for (let i = 0; i < colNum; i++) {
@@ -19,5 +22,19 @@ function setGame() {
     }
     tableEle += '</table>';
     document.getElementById("gameBoard").innerHTML = tableEle;
-    }  
 }
+
+function setMineNumArr(numLimit, numRange) {
+    mines = [];
+    for (let i = 0; i < numLimit; i++) {
+        let randomNum = Math.floor(Math.random() * numRange);
+        if (!findMine(randomNum)) {
+            mines.push(randomNum);
+        } else {
+            i--;
+        }
+    }
+    function findMine(j) {
+        return mines.find((e) => (e === j));
+    }
+}   
