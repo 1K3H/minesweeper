35efde1135079053f4c511edd3dc5a4806682584
1K3H <1K3H@users.noreply.github.com>
2020-11-22 07:54:56 +0900

    타일 클릭 이벤트리스너 추가하는 함수

--- program.js
+++ program.js
@@ -9,10 +9,47 @@ function setGame() {
     const mineArr = setMineNumArr(mineNum, row * col);
     
     makeBoard(row, col);
-    
     putMineInBoard(mineArr);
+
+    // 타일에 이벤트 넣기
+
+    // 코너 타일 이벤트
+    // 좌상
+    tileEvent(mineArr, 0, 1, row, row + 1);
+    // 우상
+    tileEvent(mineArr, row - 1, row - 2, 2 * row - 2, 2 * row - 1);
+    // 좌하
+    tileEvent(mineArr, row * (col - 1), row * (col - 2), row * (col - 2) + 1, row * (col - 1) + 1);
+    // 우하
+    tileEvent(mineArr, row * col - 1, row * (col - 1) - 2, row * (col - 1) - 1, row * col - 2);
+
+    // 모서리 타일
+    // 상
+    for (let i = 1; i <= row - 2; i++) {
+        tileEvent(mineArr, i, i - 1, i + 1, i + row - 1, i + row, i + row + 1);
+    }
+    // 하
+    for (let i = row * (col - 1) + 1; i <= row * col - 2; i++) {
+        tileEvent(mineArr, i, i - row - 1, i - row, i - row + 1, i - 1, i + 1);
+    }
+    // 좌
+    for (let i = row; i <= row * (col - 2); i += row) {
+        tileEvent(mineArr, i, i - row, i - row + 1, i + 1, i + row, i + row + 1);
+    }
+    // 우
+    for (let i = 2 * row - 1; i <= row * (col - 1) - 1; i += row) {
+        tileEvent(mineArr, i, i - row - 1, i - row, i - 1, i + row - 1, i + row);
+    }
+
+    // 나머지 모든 타일
+    for (let i = 1; i <= col - 2; i++) {
+        for (let j = i * row + 1; j <= (1 + i) * row - 2; j++) {
+            tileEvent(mineArr, j, j - row - 1, j - row, j - row + 1, j - 1, j + 1, j + row - 1, j + row, j + row + 1);
+        }
+    }
 }
 
+// board 만들기
 function makeBoard(rowNum, colNum) {
     let tableEle = '<table>';
 
@@ -27,6 +64,7 @@ function makeBoard(rowNum, colNum) {
     document.getElementById("gameBoard").innerHTML = tableEle;
 }
 
+// 지뢰 위치 번호 뽑기
 function setMineNumArr(numLimit, numRange) {
     let mineArr = [];
     for (let i = 0; i < numLimit; i++) {
@@ -43,6 +81,7 @@ function setMineNumArr(numLimit, numRange) {
     return mineArr;
 }
 
+// board에 'mine' class로 삽입하기 
 function putMineInBoard(mine) {
     for (let i = 0; i < tdArr.length; i++) {
         if (findMine(i)) {
@@ -52,4 +91,31 @@ function putMineInBoard(mine) {
     function findMine(j) {
         return mine.find((e) => (e === j));
     }
+}
+
+// 타일 클릭 시 실행할 함수 추가
+function tileEvent(mine ,targetNum, ...aroundArr) {
+    tdArr[targetNum].addEventListener("click", function () {
+        let count = 0;
+        for (let i = 0; i < aroundArr.length; i++) {
+            if (findMine(aroundArr[i])) {
+                count++
+            }
+        }
+        function findMine(j) {
+            return mine.find((e) => (e === j));
+        }
+        if (tdArr[targetNum].className === 'mine') {
+            alert('GAME OVER!!!')
+        }
+        else if (count === 0) {
+            tdArr[targetNum].style.backgroundColor = "darkcyan";
+            for (let i = 0; i < aroundArr.length; i++) {
+                tdArr[aroundArr[i]].click();
+            }
+        }
+        else {
+            tdArr[targetNum].innerHTML = count;
+        }
+    })
 }
